09434bb93862106d9476575f34ec68045933bb9f
Jan Rothen <jan.rothen@me.com>
2025-08-19 11:04:45 +0200

    removed ledstriplight (moved to its own repo)

--- ledstriplight/src/config/pin_assignment.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-from typing import NamedTuple
-
-class PinAssignment(NamedTuple):
-    """Holds GPIO pin assignments for red, green, and blue channels."""
-    red: int
-    green: int
-    blue: int
